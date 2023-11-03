bbfdae10654c7057d28973c3a7b26b31afb50079
Kevin <pikboy123@gmail.com>
2023-11-03 10:39:12 -0400

    Building out 'all_blogs'

--- portfolio_project/blog/models.py
+++ portfolio_project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)    
+    body = models.TextField(editable=True)
+    date = models.DateTimeField(auto_now_add=True)
+    url = models.URLField(blank=True)
